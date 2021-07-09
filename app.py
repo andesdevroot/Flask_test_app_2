cc4475bc3f4fb89a25a80f971910bc905ab83694
andesdevroot <andes.dev.root@gmail.com>
2021-07-09 19:06:55 -0400

    se crean rutas con variables

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,12 +6,18 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-@app.route('/home')
-def home():
-    return 'Home Page'
+#rutas con variables
+@app.route('/home', methods=['GET', ' POST'], defaults={'name': 'Default'})
+@app.route('/home/<string:name>', methods=['GET','POST'])
+def home(name):
+    return '<h1>Hola {}, tu estas en la pagina home!!</h1>'.format(name)
+
+
+@app.route('/json')
+def json():
+    return jsonify({"key": "value", 'key2': [1,2,3,4]})
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-    
+
+
